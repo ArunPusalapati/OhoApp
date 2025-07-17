2cc06467644a89c502b2adc38335b8f6342edf6e
Venkat Arun <arunkumarpv04@gmail.com>
2025-07-17 05:22:10 -0500

    added signup toggle feature

--- oho/src/components/SignIn.js
+++ oho/src/components/SignIn.js
@@ -5,7 +5,7 @@ const SignIn=()=>{
     const password=useRef(null);
     const [emailErrorMessage,setEmailErrorMessage]=useState("");
     const [passwordErrorMessage,setPasswordErrorMessage]=useState("");
-
+    const [isSignUpToggle,setIsSignUpToggle]=useState(false);
    const Validate = (e, p) => {
   const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(
     e
@@ -20,11 +20,16 @@ const SignIn=()=>{
 };
     return (
        <div className="absolute z-40 bg-black ml-155 mt-45 min-h-100 min-w-100 rounded opacity-70">
-        <h1 className="text-4xl text-red-500 font-bold m-4 ml-6">Sign In</h1>
+        {
+          !isSignUpToggle ?<h1 className="text-4xl text-red-500 font-bold m-4 ml-6">Sign In</h1>:<h1 className="text-4xl text-red-500 font-bold m-4 ml-6">Sign Up</h1>
+         }
         <Form className="flex flex-col items-center"
         onSubmit={(e)=>e.preventDefault()}
         >
-            
+            {isSignUpToggle &&  <input type="text"
+            ref={email}
+            placeholder="Name"
+            className="block border-1 text-white border-white mx-4 my-3 rounded-sm min-w-80 min-h-10 placeholder-white p-3 hover:border-2 border-outline-white" />}
             <input type="text"
             ref={email}
             placeholder="Email or Phone Number"
@@ -41,8 +46,13 @@ const SignIn=()=>{
             onClick={()=>{Validate(email.current.value,password.current.value) 
                 console.log(email.current.value+" "+password.current.value)
             }}
-            >Sign In</button>
-            <p className="text-white m-4">New to Netflix? <a className="cursor-pointer">Sign up now.</a></p>
+            >{!isSignUpToggle?"Sign In":"Sing UP"}</button>
+            {!isSignUpToggle?
+            <p className="text-white m-4">New to Netflix? <button className="cursor-pointer"
+            onClick={()=>setIsSignUpToggle(true)}
+            >Sign up now.</button></p> :<p className="text-white m-4">New to Netflix? <button className="cursor-pointer"
+            onClick={()=>setIsSignUpToggle(false)}
+            >Sign in now.</button></p>}
         </Form>
             
         </div>
